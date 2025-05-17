83cfee0dbc5cdad98876f927f00bca166a820a4c
arvinlp <arvinlp91@gmail.com>
2025-05-18 02:55:44 +0330

    ui fixing

--- resources/views/livewire/user-manager.blade.php
+++ resources/views/livewire/user-manager.blade.php
@@ -1,85 +1,43 @@
 <div>
-    <h2>{{ $isEdit ? 'Edit User' : 'Create User' }}</h2>
+    <flux:callout class="mb-6">
+        <h2>{{ $isEdit ? 'Edit User' : 'Create User' }}</h2>
 
-    <form wire:submit.prevent="{{ $isEdit ? 'update' : 'create' }}">
+        <form class="" wire:submit.prevent="{{ $isEdit ? 'update' : 'create' }}">
+            <flux:field class="mb-3">
+                <flux:input wire:model="nickname" :label="__('Nickname')" type="text" required autofocus
+                    autocomplete="nickname" placeholder="Johnny" />
+                <flux:error name="nickname" />
+            </flux:field>
+            <flux:field class="mb-3">
+                <flux:input wire:model="first_name" :label="__('First Name')" type="text" required autofocus
+                    autocomplete="given-name" placeholder="John" />
+                <flux:error name="first_name" />
+            </flux:field>
+            <flux:field class="mb-3">
+                <flux:input wire:model="last_name" :label="__('Last Name')" type="text" required autofocus
+                    autocomplete="family-name" placeholder="Doe" />
+                <flux:error name="last_name" />
+            </flux:field>
+            <flux:field class="mb-3">
+                <flux:input wire:model="mobile" :label="__('Mobile number')" type="number" required autofocus
+                    autocomplete="mobile" placeholder="0912345678" pattern="[0-9]{10}"
+                    title="Please enter a valid mobile number" inputmode="numeric" maxlength="10" minlength="10"
+                    oninput="this.value = this.value.replace(/[^0-9]/g, '')"
+                    onkeypress="return event.charCode >= 48 && event.charCode <= 57" onpaste="return false" />
+                <flux:error name="mobile" />
+            </flux:field>
+            <flux:field class="mb-3">
+                <flux:input wire:model="email" :label="__('Email address')" type="email" required autofocus
+                    autocomplete="email" placeholder="email@example.com" />
+                <flux:error name="email" />
+            </flux:field>
 
-        <flux:input
-            wire:model="nickname"
-            :label="__('Nickname')"
-            type="text"
-            required
-            autofocus
-            autocomplete="nickname"
-            placeholder="Johnny"
-        />
-        @error('nickname')
-            <span>{{ $message }}</span>
-        @enderror
-
-        <flux:input
-            wire:model="first_name"
-            :label="__('First Name')"
-            type="text"
-            required
-            autofocus
-            autocomplete="given-name"
-            placeholder="John"
-        />
-        @error('first_name')
-            <span>{{ $message }}</span>
-        @enderror
-
-        <flux:input
-            wire:model="last_name"
-            :label="__('Last Name')"
-            type="text"
-            required
-            autofocus
-            autocomplete="family-name"
-            placeholder="Doe"
-        />
-        @error('last_name')
-            <span>{{ $message }}</span>
-        @enderror
-
-        <flux:input
-            wire:model="mobile"
-            :label="__('Mobile number')"
-            type="number"
-            required
-            autofocus
-            autocomplete="mobile"
-            placeholder="0912345678"
-            pattern="[0-9]{10}"
-            title="Please enter a valid mobile number"
-            inputmode="numeric"
-            maxlength="10"
-            minlength="10"
-            oninput="this.value = this.value.replace(/[^0-9]/g, '')"
-            onkeypress="return event.charCode >= 48 && event.charCode <= 57"
-            onpaste="return false"
-        />
-        @error('mobile')
-            <span>{{ $message }}</span>
-        @enderror
-
-        <flux:input
-            wire:model="email"
-            :label="__('Email address')"
-            type="email"
-            required
-            autofocus
-            autocomplete="email"
-            placeholder="email@example.com"
-        />
-        @error('email')
-            <span>{{ $message }}</span>
-        @enderror
-        <flux:button.group>
-            <flux:button type="submit">{{ $isEdit ? 'Update' : 'Create' }}</flux:button>
-            <flux:button wire:click="resetForm">Cancel</flux:button>
-        </flux:button.group>
-    </form>
+            <flux:button.group class="mt-6">
+                <flux:button type="submit">{{ $isEdit ? 'Update' : 'Create' }}</flux:button>
+                <flux:button wire:click="resetForm" variant="ghost">Cancel</flux:button>
+            </flux:button.group>
+        </form>
+    </flux:callout>
 
     @if (session()->has('message'))
         <div>{{ session('message') }}</div>
@@ -87,7 +45,7 @@
 
     <hr>
 
-    <table class="py-2 px-3">
+    <table class="table-fixed">
         <thead>
             <tr>
                 <th>#</th>
